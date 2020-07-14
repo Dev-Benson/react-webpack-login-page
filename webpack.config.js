4d29ab40d0ba2b583ce14d91ebff10084a1e0a1a
Nockk-B <ebrusontop@gmail.com>
2020-07-14 23:19:36 +0100

    configure webpack Subject
    Problem
    
    Solution
    
    Note

---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+const path = require("path");
+const Htmlplugin = require("html-webpack-plugin")
+
+module.exports ={
+    entry: "./src/index.js",
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "bundle.js"
+    },
+    devServer: {
+        contentBase: path.resolve(__dirname, 'dist'),
+        port: 9000
+    },
+    module: {
+      rules: [
+          {
+            test: /\.(js|jsx)$/,
+            exclude: /node_modules/,
+            loader: "babel-loader",
+            options: {
+              presets: [
+                "@babel/preset-env",
+                "@babel/preset-react"
+              ],
+              plugins: [
+                "@babel/transform-runtime"
+              ]
+            }
+          },
+          
+          {
+            test: /\.(css|scss)$/,
+            use: [ 
+              'style-loader',
+              {
+                loader: "css-loader"
+              }
+            ],
+          },
+          {
+            test: /\.(png|svg|jpg|gif|webp|woff(2)?|ttf|eot|svg)$/,
+            use: ['file-loader']
+          }
+          
+        ]  
+    },
+    plugins: [
+      new Htmlplugin({template: "./src/index.html"})
+    ]
+}
